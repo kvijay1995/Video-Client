9efc14e908bd461e4a2f19ff3e16f69c503fc919
Vijay Thiagarajan <vkthiaga@ncsu.edu>
2015-07-23 23:08:02 -0400

    Stopped sending confirmations to the server and changed input stream reading format

--- src/main/java/com/mycompany/Video_Client/MainActivity.java
+++ src/main/java/com/mycompany/Video_Client/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 
 import java.io.DataOutputStream;
@@ -50,59 +51,64 @@ public class MainActivity extends Activity {
 
     public void ConnectToServer(View view)
     {
+        // Get textbox reference
         EditText textObj = (EditText) findViewById(R.id.ServerInfo);
         String s = textObj.getText().toString();
-        if(s.contains(" "))
+        int ind = s.indexOf(" ");
+
+        // Find port and server address.
+        final String serverAddress = s.substring(0, ind).trim();
+        String port_s = s.substring(ind).trim();
+        final int port = Integer.valueOf(port_s);
+
+        // Start a new thread to avoid NetworkOnMainThreadException
+        Thread thread = new Thread(new Runnable()
         {
-            int ind = s.indexOf(" ");
-            final String serverAddress = s.substring(0, ind).trim();
-            String port_s = s.substring(ind).trim();
-            final int port = Integer.valueOf(port_s);
-
-            // Start a new thread to avoid NetworkOnMainThreadException
-            Thread thread = new Thread(new Runnable()
+            @Override
+            public void run()
             {
-                @Override
-                public void run()
+                try
                 {
-                    try
-                    {
-                        Socket socket = new Socket(serverAddress, port);
+                    Socket socket = new Socket(serverAddress, port);
 
-                        int i = 0;
-                        while (i < 5000)
+                    // Read the buffer size and send receive confirm
+                    DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+
+                    int i = 0;
+                    // TODO: change this while loop to listen to a "disconnect" button event
+                    while (i < 5000)
+                    {
+                        // Read image size and look for line feed to denote the start of image
+                        ByteArrayOutputStream c = new ByteArrayOutputStream();
+                        int j = dis.readByte();
+                        while(j != 10)
                         {
-                            // Read the buffer size and send receive confirm
-                            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                            String str = br.readLine();
-                            int buffer_size = Integer.parseInt(str);
-                            dos.writeBytes("Y");
-
-                            // Read image as stream of bytes
-                            DataInputStream dis = new DataInputStream(socket.getInputStream());
-
-                            byte[] b = new byte[buffer_size];
-                            dis.readFully(b);
-                            dos.writeBytes("V");
-
-                            // TODO: find a way to remove the final keyword for bitmap
-                            // Convert to bitmap and update UI
-                            final Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    ImageView picture = (ImageView) findViewById(R.id.imageDsp);
-                                    picture.setImageBitmap(bitmap);
-                                }
-                            });
-                            i++;
+                            c.write(j);
+                            j = dis.readByte();
                         }
+                        int buffer_size = Integer.parseInt(c.toString());
+
+                        // Read image as stream of bytes
+                         byte[] b = new byte[buffer_size];
+                        dis.readFully(b);
+
+                        // TODO: find a way to remove the final keyword for bitmap
+                        // Convert to bitmap and update UI
+                        final Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ImageView picture = (ImageView) findViewById(R.id.imageDsp);
+                                picture.setImageBitmap(bitmap);
+                            }
+                        });
+                        i++;
                     }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
                 }
             });
             thread.start();
